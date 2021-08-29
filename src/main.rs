8326ddefa724835d4eeffeb4ca643468bc31b5ff
Alexander Van Hecke <alexander@avhconsult.be>
2021-08-29 20:10:58 +0200

    rewrite using byte buffers, this results in a 30x speedup.  UTF-8 string handling for big strings is slow.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::io::{BufReader, BufWriter, Write};
 use std::{io, io::prelude::*};
 use structopt::StructOpt;
-use std::io::{BufWriter, Write};
+
+#[macro_use]
+extern crate lazy_static;
 
 pub type Error = Box<dyn std::error::Error + Send + Sync>;
 pub type Result<T> = std::result::Result<T, Error>;
@@ -9,7 +12,11 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[structopt(name = "pgdump_filter")]
 struct Options {
     /// Exclude the listed copy block(s)
-    #[structopt(short = "e", long = "excluded_copy_blocks", conflicts_with="included_copy_blocks")]
+    #[structopt(
+    short = "e",
+    long = "excluded_copy_blocks",
+    conflicts_with = "included_copy_blocks"
+    )]
     excluded_copy_blocks: Vec<String>,
     /// Include the listed copy block(s)
     #[structopt(short = "i", long = "included_copy_blocks")]
@@ -20,9 +27,16 @@ struct Options {
     /// Schema of the objects
     #[structopt(default_value = "public", short = "s", long = "schema")]
     schema: String,
+    #[structopt(
+    default_value = "32",
+    short = "b",
+    long = "buffersize",
+    help = "size of buffer in MB.  make sure this is enough to hold the longest line in the file."
+    )]
+    buffersize_in_mb: usize,
 }
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Copy)]
 pub enum State {
     Init,
     Comment,
@@ -34,34 +48,58 @@ pub enum State {
     Statement,
 }
 
+lazy_static! {
+    static ref COMMENT: &'static [u8] = b"--";
+    static ref END_OF_COPY_BLOCK: &'static [u8] = b"\\.";
+    static ref NEWLINE: &'static [u8] = b"\n";
+    static ref COPY_BLOCK_PREFIX: &'static [u8] = b"COPY ";
+    static ref COPY_BLOCK_SUFFIX: &'static [u8] = b"FROM stdin;\n";
+    static ref LO_CREATE: &'static [u8] = b"SELECT pg_catalog.lo_create";
+    static ref LO_FN: &'static [u8] = b"SELECT pg_catalog.lo_";
+    static ref LO_WRITE: &'static [u8] = b"SELECT pg_catalog.lowrite";
+}
 impl State {
-    fn next_state(&self, line: &String, opts: &Options) -> State {
-        match line {
-            l if l.is_empty() => match self {
-                State::EmptyLine => State::ConsecutiveEmptyLine,
-                State::ConsecutiveEmptyLine => State::ConsecutiveEmptyLine,
-                _ => State::EmptyLine
-            }
-            l if l.starts_with("--") => State::Comment,
-            l if l.starts_with("COPY ") && l.ends_with("FROM stdin;") && opts.excluded_copy_blocks
-                .iter()
-                .find(|&excluded_block| l.to_lowercase().contains(&format!("COPY {}.{} ", opts.schema, excluded_block).to_lowercase()))
-                .is_some() => State::ExcludedCopyBlock,
-            l if l.starts_with("COPY ") && l.ends_with("FROM stdin;") && !opts.included_copy_blocks.is_empty() && opts.included_copy_blocks
-                .iter()
-                .find(|&included_block| l.to_lowercase().contains(&format!("COPY {}.{} ", opts.schema, included_block).to_lowercase()))
-                .is_none() => State::ExcludedCopyBlock,
-            l if l.starts_with("\\.") => match self {
-                State::ExcludedCopyBlock => State::EndOfExcludedCopyBlock,
-                state => state.clone()
-            },
+    fn next_state(&self, buf: &[u8], opts: &Options) -> Result<State> {
+        match buf {
             // keep the lo_create calls (oid colums in tables must work)
-            l if l.to_lowercase().starts_with("select pg_catalog.lo_create") => State::Statement,
-            l if l.to_lowercase().starts_with("select pg_catalog.lo_") ||  l.to_lowercase().starts_with("select pg_catalog.lowrite") => State::LargeObject,
-            _ => match self {
-                State::ExcludedCopyBlock => State::ExcludedCopyBlock,
-                _ => State::Statement
+            buf if buf.starts_with(*LO_CREATE) => Ok(State::Statement),
+            buf if buf.starts_with(*LO_FN) || buf.starts_with(*LO_WRITE) => Ok(State::LargeObject),
+            buf if buf.starts_with(*NEWLINE) => match self {
+                State::EmptyLine => Ok(State::ConsecutiveEmptyLine),
+                State::ConsecutiveEmptyLine => Ok(State::ConsecutiveEmptyLine),
+                _ => Ok(State::EmptyLine),
+            },
+            buf if buf.starts_with(*COMMENT) => Ok(State::Comment),
+            buf if buf.starts_with(*END_OF_COPY_BLOCK) => match self {
+                State::ExcludedCopyBlock => Ok(State::EndOfExcludedCopyBlock),
+                state => Ok(*state),
+            },
+            buf if buf.starts_with(*COPY_BLOCK_PREFIX) && buf.ends_with(*COPY_BLOCK_SUFFIX) => {
+                let l = String::from_utf8(buf.to_vec())?.to_lowercase();
+                match l {
+                    l if opts.excluded_copy_blocks.iter().any(|excluded_block| {
+                        l.contains(
+                            &format!("COPY {}.{} ", opts.schema, excluded_block).to_lowercase(),
+                        )
+                    }) => Ok(State::ExcludedCopyBlock),
+                    l if !opts.included_copy_blocks.is_empty()
+                        && opts
+                        .included_copy_blocks
+                        .iter()
+                        .find(|&included_block| {
+                            l.to_ascii_lowercase().contains(
+                                &format!("COPY {}.{} ", opts.schema, included_block)
+                                    .to_lowercase(),
+                            )
+                        })
+                        .is_none() => Ok(State::ExcludedCopyBlock),
+                    _ => Ok(State::Statement),
+                }
             }
+            _ => match self {
+                State::ExcludedCopyBlock => Ok(State::ExcludedCopyBlock),
+                _ => Ok(State::Statement),
+            },
         }
     }
 
@@ -84,22 +122,27 @@ pub fn main() -> Result<()> {
     let mut state: State = State::Init;
 
     let stdout = std::io::stdout();
-    let lock = stdout.lock();
-    let mut out = BufWriter::new(lock);
+    let stdout = stdout.lock();
+    let mut stdout = BufWriter::with_capacity(64 * 1024, stdout);
 
-    for line in io::stdin().lock().lines() {
-        match line {
-            Ok(line) => {
-                state = state.next_state(&line, &opts);
-                if state.must_include(&opts, &prev_included_state) {
-                    prev_included_state = state.clone();
-                    writeln!(out, "{}", line)?;
-                }
-            }
-            Err(e) => panic!("An IO error occurred {}", e)
+    let reader = io::stdin();
+    let reader = reader.lock();
+    let mut reader = BufReader::with_capacity(64 * 1024, reader);
+    let mut buf: Vec<u8> = Vec::with_capacity(opts.buffersize_in_mb * 1024 * 1024);
+
+    loop {
+        buf.clear();
+
+        let number_of_bytes_read = reader.read_until(b'\n', &mut buf)?;
+        if number_of_bytes_read == 0 {
+            break;
+        }
+        state = state.next_state(&buf, &opts)?;
+        if state.must_include(&opts, &prev_included_state) {
+            prev_included_state = state;
+            stdout.write_all(&buf)?;
         }
     }
-    out.flush()?;
+    stdout.flush()?;
     Ok(())
 }
-
